b824022f8785d8eb845afe613e6fe89fdf5f9171
Pip Murphy <santashi.finn@gmail.com>
2024-11-25 12:19:50 +0000

    kata 3 get api topics completed

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 
 const { getApi } = require("./controllers/api.controller");
+const { getTopics } = require("./controllers/topics.controller");
 
 app.use(express.json());
 
 app.get("/api", getApi);
+
+app.get("/api/topics", getTopics);
+
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Not found" });
+});
 
 module.exports = app;

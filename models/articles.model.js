1fe086e2db3a4d32ad41e9299c9faaa2f65f7946
Pip Murphy <santashi.finn@gmail.com>
2024-11-26 10:03:40 +0000

    kata 6 get all comments for an article completed

--- models/articles.model.js
+++ models/articles.model.js
@@ -30,3 +30,29 @@ exports.selectArticleById = (article_id) => {
       return rows[0];
     });
 };
+
+exports.selectCommentsByArticle = (article_id) => {
+  return db
+    .query(
+      `SELECT * FROM comments
+      WHERE comments.article_id = $1
+      ORDER BY created_at DESC;`,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
+
+exports.checkArticleExists = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({
+          status: 404,
+          msg: "Not found",
+        });
+      }
+    });
+};

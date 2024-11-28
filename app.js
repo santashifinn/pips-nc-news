61fa04dfbd35f78a478ac75bd7889c9aa93dc97c
Pip Murphy <santashi.finn@gmail.com>
2024-11-28 11:18:28 +0000

    kata 16 express routers completed

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 const express = require("express");
 const app = express();
-
-const { getApi } = require("./controllers/api.controller");
-const { getTopics } = require("./controllers/topics.controller");
-const {
-  getArticles,
-  getArticleById,
-  updateVotes,
-} = require("./controllers/articles.controller");
-const {
-  getCommentsByArticle,
-  postComment,
-  deleteComment,
-} = require("./controllers/comments.controller");
-const { getUsers } = require("./controllers/users.controller");
+const apiRouter = require("./routes/api-router");
+const topicsRouter = require("./routes/topics-router");
+const articlesRouter = require("./routes/articles-router");
+const usersRouter = require("./routes/users-router");
+const commentsRouter = require("./routes/comments-router");
 
 app.use(express.json());
 
-app.get("/api", getApi);
-
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles", getArticles);
-app.get("/api/articles/:articles_id", getArticleById);
-
-app.get("/api/articles/:articles_id/comments", getCommentsByArticle);
-app.post("/api/articles/:article_id/comments", postComment);
-app.delete("/api/comments/:comment_id", deleteComment);
-
-app.patch("/api/articles/:article_id", updateVotes);
-
-app.get("/api/users", getUsers);
+app.use("/api", apiRouter);
+app.use("/api/topics", topicsRouter);
+app.use("/api/articles", articlesRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/users", usersRouter);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Not found" });

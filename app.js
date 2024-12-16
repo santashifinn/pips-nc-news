841817aaf9aa4cae215bd6361a50cb98c65811f1
Pip Murphy <santashi.finn@gmail.com>
2024-12-16 11:01:18 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const apiRouter = require("./routes/api-router");
 const topicsRouter = require("./routes/topics-router");
 const articlesRouter = require("./routes/articles-router");
@@ -10,6 +11,8 @@ const {
   postgresErrorHandler,
   customErrorHandler,
 } = require("./errors");
+
+app.use(cors());
 
 app.use(express.json());
 
